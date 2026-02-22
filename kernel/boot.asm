e126c3b59c714e2bf7b000b52468a4d51fa07faf
luthervd <luke.mcg79@gmail.com>
2026-02-22 22:47:49 +0000

    CPU checks done ready for paging tables and long mode GDT

--- kernel/boot.asm
+++ kernel/boot.asm
@@ -1,41 +1,27 @@
 [org 0x7c00]
     KERNEL_OFFSET equ 0x1000
-
+    CPUID_EXTENSIONS equ 0x80000000 ; returns the maximum extended requests for cpuid
+    CPUID_FEATURES equ 0x80000001 ; returns flags containing long mode support among other things
     mov [BOOT_DRIVE], dl
-
     mov bp, 0x9000
     mov sp, bp
-
-    mov bx, MSG_REAL_MODE
-    call print_string
-
     call switch_to_pm
-
     jmp $
-
-%include "../functions/print_string.asm"
 %include "../protected_mode/gdt.asm"
-%include "../functions/disk_load.asm"
-
 
 [bits 16]
 ; switch to protected mode
 switch_to_pm:
-    mov bx, MSG_PRE_JUMP
-    call print_string
-
     cli  ; switch off interputs
     lgdt [gdt_descriptor]
-
     mov eax, cr0
     or eax, 0x1                ; To make the switch to protected mode , we set
     mov cr0, eax               ; the first bit of CR0 , a control register
-
     jmp CODE_SEG:init_pm ; Jump to new 32 bit segment which will flush CPU cache as well
 
 [bits 32]
-; init registers and the stack once in PM
 %include "../protected_mode/print.asm"
+%include "../protected_mode/check_cpu.asm"
 init_pm:
 
     mov ax, DATA_SEG    ; Now in PM , our old segments are meaningless ,
@@ -48,17 +34,35 @@ init_pm:
     mov ebp, 0x90000     ; Set stack position to top of free space
     mov esp, ebp
 
-    mov ebx, MSG_PROT_MODE
-    call print_string_pm
+    call checkCPUID ; check cpuid
+    cmp ax, 1
+    je .queryLongMode
+    jmp .noLongMode
 
+    .queryLongMode:
+        mov eax, CPUID_EXTENSIONS
+        cpuid
+        cmp eax, CPUID_FEATURES
+        jb .noLongMode    
+    
+    .longModeSupported:
+        mov ebx, MSG_LONG_MODE_SUPPORTED
+        call print_string_pm
+        jmp .done
+
+    .noLongMode:
+        mov ebx, MSG_NO_LONG_MODE
+        call print_string_pm
+        jmp .done  
+
+    .done: 
     jmp $
 
 ; Global variables
 BOOT_DRIVE db 0
-MSG_REAL_MODE db "Started in 16 - bit Real Mode" , 0
-MSG_PRE_JUMP db "Jumping to protected mode..." , 0
-MSG_PROT_MODE db "Successfully landed in 32 - bit Protected Mode" , 0
-MSG_LOAD_KERNEL db "Loading kernel into memory." , 0
+
+MSG_NO_LONG_MODE db "Long Mode NOT Supported" , 0
+MSG_LONG_MODE_SUPPORTED db "Long Mode Supported" , 0
 
 ; Bootsector padding
 times 510-($-$$) db 0
